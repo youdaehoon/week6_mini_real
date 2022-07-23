2a5368462b1e37506ce02d158344b1a46226b234
Hyunoh Jo <hyunoh.jo@gmail.com>
2022-07-23 11:24:10 +0900

    test

---
+++ src/test.js
@@ -0,0 +1,32 @@
+function addWords(wordData) {
+    return async (dispatch) => {
+      const docRef = await addDoc(collection(db, "mydic"), wordData);
+      // const new_word = { id: docRef.id, ...wordData.data() };
+      dispatch({ type: "WORD/ADD_WORD", payload: new_word });
+    };
+  }
+  
+  function editWords(wordData) {
+    return async (dispatch) => {
+      const docRef = await doc(db, "mydic", wordData.id);
+      await updateDoc(docRef, wordData);
+      // dispatch(wordActions.editWord(wordData));
+    };
+  }
+  
+  function doneWords(id,completed) {
+    return async (dispatch) => {
+      const docRef = await doc(db, "mydic", id);
+      await updateDoc(docRef, completed?{completed:false}:{completed:true});
+      dispatch(wordSliceActions.doneWord({id, completed}));
+    };
+  }
+  
+  function delWords(id) {
+    return async (dispatch) => {
+      const docRef = await doc(db, "mydic", id);
+      await deleteDoc(docRef);
+      // dispatch({ type: "WORD/DEL_WORD", payload: { id } });
+      dispatch(wordSliceActions.delWord({id}));
+    };
+  }
